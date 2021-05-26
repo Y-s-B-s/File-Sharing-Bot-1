cd44fa44add7d6b03cbb1d756cef47256550b431
Achu biju <75154223+Achu2234@users.noreply.github.com>
2021-05-26 09:13:56 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/CodeXBotz/File-Sharing-Bot'>Click here</a>\n○ Channel : @CodeXBotz\n○ Support Group : @CodeXBotzSupport</b>",
+            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>Achu biju</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {@__version__}</a>\n○ Bot update : <a href='https://t.me/Animemusicarchive6'>Click here</a>\n○ Channel : @MWU_TG\n○ Support Group : @MWU_TG_REQUEST</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
